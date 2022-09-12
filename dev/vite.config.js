5c10964fa67e3184717e96133f915245b7f07c1a
Milan Hauth <milahu@gmail.com>
2022-09-12 20:49:02 +0200

    fix build options

--- dev/vite.config.js
+++ dev/vite.config.js
@@ -1,3 +1,15 @@
+const assetsDir = 'assets/';
+//const assetsDir = '';
+
+const outputDefaults = {
+  format: 'iife',
+
+  // remove hashes from filenames
+  entryFileNames: `${assetsDir}[name].js`,
+  chunkFileNames: `${assetsDir}[name].js`,
+  assetFileNames: `${assetsDir}[name].[ext]`,
+}
+
 export default {
   plugins: [],
   root: "dev",
@@ -6,5 +18,26 @@ export default {
     hmr: {
       //port: 443,
     }
-  }
+  },
+  build: {
+    target: "esnext",
+    polyfillDynamicImport: false,
+    //sourcemap: true,
+    minify: false, // boolean | 'terser' | 'esbuild'
+    // false: 5396.78 KiB // smaller git diffs
+    // 'esbuild': 2027.36 KiB // default
+    // 'terser': 2002.37 KiB
+    rollupOptions: {
+      output: {
+        ...outputDefaults,
+      }
+    },
+  },
+  worker: {
+    rollupOptions: {
+      output: {
+        ...outputDefaults,
+      }
+    },
+  },
 }
