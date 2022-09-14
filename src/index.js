6ddb3a7923f06a47d169d435da16ae4f1ea11d7d
Milan Hauth <milahu@gmail.com>
2022-09-15 00:45:55 +0200

    fix tokens

--- src/index.js
+++ src/index.js
@@ -44,14 +44,16 @@ export const nixLanguage = LRLanguage.define({
         TRUE: t.bool,
         FALSE: t.bool,
         String: t.string,
+        StringContent: t.string,
         IndentedString: t.string,
+        IndentedStringContent: t.string,
         Comment: t.lineComment,
         CommentBlock: t.blockComment,
         Float: t.float,
         Integer: t.integer,
         NULL: t.null,
         URI: t.url,
-        //SPath: t.literal, // TODO what is stringpath?
+        //SPath: t.literal, // TODO what is stringpath? not implemented in nix.grammar
         Path: t.literal,
         "( )": t.paren,
         "{ }": t.brace,
