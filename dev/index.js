8a5cd99df17b3450a1f5b15e8306bd20c91b5528
Milan Hauth <milahu@gmail.com>
2023-12-28 20:21:49 +0100

    disable printTree

--- dev/index.js
+++ dev/index.js
@@ -3,7 +3,7 @@ import { EditorView } from '@codemirror/view';
 import { basicSetup } from 'codemirror';
 import { nix } from "../dist/";
 import { parser } from "lezer-parser-nix";
-import { printTree } from "./print-lezer-tree";
+//import { printTree } from "./print-lezer-tree";
 import { oneDark } from '@codemirror/theme-one-dark';
 
 const doc = `
@@ -51,4 +51,4 @@ new EditorView({
   parent: document.querySelector('#editor'),
 });
 
-console.log(printTree(parser.parse(doc), doc));
+//console.log(printTree(parser.parse(doc), doc));
