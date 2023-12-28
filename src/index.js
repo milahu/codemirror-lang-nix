215bbaebe6a610d8cbe37c1a1d05311fcb258f37
Milan Hauth <milahu@gmail.com>
2023-12-28 19:09:54 +0100

    remove styleTags props
    moved to highlight.js in lezer-parser-nix

--- src/index.js
+++ src/index.js
@@ -10,8 +10,6 @@ import {
   continuedIndent,
 } from "@codemirror/language";
 
-import { styleTags, tags as t } from "@lezer/highlight";
-
 export const nixLanguage = LRLanguage.define({
   parser: parser.configure({
     props: [
@@ -30,28 +28,6 @@ export const nixLanguage = LRLanguage.define({
           if (!first || !last) return null;
           return { from: first.to, to: last.from };
         },
-      }),
-      styleTags({
-        Identifier: t.propertyName,
-        TRUE: t.bool,
-        FALSE: t.bool,
-        String: t.string,
-        StringContent: t.string,
-        IndentedString: t.string,
-        IndentedStringContent: t.string,
-        Comment: t.lineComment,
-        CommentBlock: t.blockComment,
-        Float: t.float,
-        Integer: t.integer,
-        NULL: t.null,
-        URI: t.url,
-        //SPath: t.literal, // TODO what is stringpath? not implemented in nix.grammar
-        Path: t.literal,
-        "( )": t.paren,
-        "{ }": t.brace,
-        "[ ]": t.squareBracket,
-        "if then else": t.controlKeyword,
-        "import with let in rec builtins inherit assert or": t.keyword,
       }),
     ],
   }),
